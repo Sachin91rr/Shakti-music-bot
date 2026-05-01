f8ac714885387e7b5c8a1acb2d1ee70efd1d7d6f
@ytsaloni_sachin <142960455+Sachin91rr@users.noreply.github.com>
2026-05-01 12:25:29 +0530

    Create database.py

---
+++ handlers/handlers/handlers/core/core/core/handlers/handlers/handlers/core/core/core/core/database.py
@@ -0,0 +1,6 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
+
+mongo = AsyncIOMotorClient(MONGO_DB_URI)
+db = mongo["shakti_music"]
+playlists = db["playlists"]
